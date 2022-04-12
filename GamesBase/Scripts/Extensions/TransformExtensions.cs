7f34819d27948b01ac092342f83777090ca93e71
DejaMooGames <DejaMooGames@gmail.com>
2022-04-12 15:47:10 -0500

    DM: Added Transform Extensions

---
+++ GamesBase/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DejaMoo.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static List<GameObject> GetAllChildren(this Transform parent)
+        {
+            var children = new List<GameObject>();
+            for (var i = 0; i < parent.childCount; i++)
+            {
+                var child = parent.GetChild(i).gameObject;
+                children.Add(child);
+                children.AddRange(GetAllChildren(child.transform));
+            }
+
+            return children;
+        }
+    }
+}
